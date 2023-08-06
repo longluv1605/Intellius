027ce56b69910d3f54254054476cf9e89a1eda82
Long <longtrong53@gmail.com>
2023-08-06 08:38:48 +0700

    demo final 1

--- Assets/Scripts/Loadgame.cs
+++ Assets/Scripts/Loadgame.cs
@@ -3,12 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Loadgame : MonoBehaviour
 {
+    public static Loadgame Instance { get; private set; }
+    private string[] positions;
+    private string[] questions;
+    private string documentary;
+    private int numberOfQuestions;
+
     public GameObject questPrefab;
+
     void Start()
     {
+        /*
         string filePath = Application.dataPath + "/Position.txt";
         try
         {
@@ -27,7 +36,7 @@ public class Loadgame : MonoBehaviour
 
                     GameObject quest = Instantiate(questPrefab);
                     quest.transform.position = new Vector3((float) x, (float) y, 1);
-                    quest.name = "quest" + i.ToString();
+                    quest.name = "Quest" + i.ToString();
                     i++;
                     
                 }
@@ -37,11 +46,49 @@ public class Loadgame : MonoBehaviour
         {
             Console.WriteLine("File not found.");
         }
+        */
+        Instance = this;
+
+        positions = File.ReadAllLines(Application.dataPath + "/Position.txt");
+        questions = File.ReadAllLines(Application.dataPath + "/Quest.txt");
+        documentary = File.ReadAllText(Application.dataPath + "/Document.txt");
+        numberOfQuestions = int.Parse(questions[0]);
+
+        int q = 0;
+        for (int i = 1; i < questions.Length; i++) 
+        {
+            if (questions[i] == "|")
+            {
+                ++i;
+                string questText = "";
+                while (questions[i + 1] != "|") 
+                {
+                    questText += questions[i] += "\n";
+                    ++i;
+                }
+                
+                char trueAns = char.Parse(questions[i++]);
+                
+                string[] parts = positions[q++].Split() as string[];
+
+                float.TryParse(parts[0], out float x);
+                float.TryParse(parts[1], out float y);
+
+                GameObject quest = Instantiate(questPrefab);
+                quest.transform.position = new Vector3((float)x, (float)y, 1);
+                quest.name = "Quest" + q.ToString();
+                quest.transform.GetComponent<Quest>().setInput(questText, trueAns);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public string getDocument()
     {
-        
+        return documentary;
+    }
+
+    public int getNumberOfQuestions()
+    {
+        return numberOfQuestions;
     }
 }
--- Assets/Scripts/Quest.cs
+++ Assets/Scripts/Quest.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Quest : MonoBehaviour
+public class Quest : MonoBehaviour
 {
     protected bool onClicked = false;
     protected string quesTxt;
